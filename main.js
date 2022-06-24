86b2813fffff1ac8865277e26a09a63fef02058a
Damilare Adegboye <damilareadegboye2000@gmail.com>
2022-06-24 12:21:52 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+//  a javascript code that prints out your name, height, and country on the screen
+alert("this is a javascript code that prints out my name, height, and country on the screen");
+
+const myName = 'Damilare Adegboye'
+const height = '160cm'
+const country = 'Nigeria'
+
+document.write(`
+NAME: ${myName}
+HEIGHT: ${height}
+COUNTRY: ${country}
+`) 
+
+console.log(`
+Name: ${myName}
+Height: ${height}
+Country: ${country}
+`)
